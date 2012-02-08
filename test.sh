c9739b69a97a7ca8a18db61ad3c37f87b9228a48
Jared Jonckheere <jonckh10@msu.edu>
2012-02-07 20:40:54 -0500

    quick fix: links on family details page
    The links on the family details page were broken by a formating change, fixed

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-for test in :
+tests=( "add hi" "set hi longname=hello" "set hi familyname=hi" "query -a" "query -a -f" "delete hi" "delete -f hi")
+for test in "${tests[@]}"
+do
+   echo ruby admin.rb  $test
+   ruby admin.rb $test
+   echo 
+done 
