3cc678431366383b09def15b75f35568b5375738
Jared Jonckheere <jonckh10@msu.edu>
2012-04-10 17:24:31 -0400

    Fixed port problem in daemon. Port hardcoded for now.

--- serverDaemon.rb
+++ serverDaemon.rb
@@ -8,9 +8,13 @@ require 'filter'
 
 require 'rubygems'
 require 'daemons'
+#reference;
+#http://www.kalzumeus.com/2010/01/15/deploying-sinatra-on-ubuntu-in-which-i-employ-a-secretary/
+
+port = 8080
 
 pwd = Dir.pwd
    Daemons.run_proc('server.rb', {:dir_mode => :normal, :dir => "/home/jonckheere/opts/"}) do
 Dir.chdir(pwd)
-exec "ruby server.rb"
+exec "ruby server.rb -env production -p #{port}"
 end
